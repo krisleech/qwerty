cb959093281ca63180e2f6a5de4655da494ebf08
Kris Leech <kris.leech@interkonect.com>
2011-09-09 20:03:09 +0100

    Add some more complexity to the sitemap

--- test/dummy/config/sitemap.rb
+++ test/dummy/config/sitemap.rb
@@ -1,7 +1,22 @@
 Sitemap.build do | root |
   root.add(:blog) do | blog |
-    blog.add(:page) do | page |
+    blog.add(:post) do | post |
+      post.set(:per_page => 10)
+      post.set(:sort => 'published_at asc')
+    end
+  end
+
+  root.add(:about) do | about |
+    about.add(:page) do | page |
       page.set(:per_page => 10)
+      page.set(:sort => 'title asc')
+    end
+  end
+
+  root.add(:home) do | home |
+    home.add(:banner) do | banner |
+      banner.set(:sort => :random)
+      banner.set(:pagination => :off)
     end
   end
 end
