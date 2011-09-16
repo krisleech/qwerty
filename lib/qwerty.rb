944dc61c3d5bd71cf389a31ef394d2805108247f
Kris Leech <kris.leech@interkonect.com>
2011-09-16 09:04:32 +0100

    Add configurability for Qwerty

--- lib/qwerty.rb
+++ lib/qwerty.rb
@@ -1,4 +1,22 @@
 require "qwerty/engine"
 
 module Qwerty
+  class << self
+
+    # Provides configurability to Qwerty
+    # * admin_title: The title shown in the admin UI
+    def options
+      @options ||= {
+        :admin_title => 'Qwerty CMS'
+      }
+    end
+
+    # Allow for easier settings of multiple options in an initalizer
+    # Qwerty.configure do | config |
+    #   config.admin_title = 'Administration'
+    # end
+    def configure
+      yield(self) if block_given?
+    end
+  end
 end
