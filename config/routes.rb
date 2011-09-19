e87d188a9c5b9c07d406efcae9a95683d32d4cf5
Kris Leech <kris.leech@interkonect.com>
2011-09-19 13:50:02 +0100

    Add Bootstrap CSS from Twitter

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,15 @@ Rails.application.routes.draw do
   resources :documents
 
   namespace :qwerty_admin do
+    root :to => 'documents#index'
+
     resources :nodes do
       member do
         get :up
         get :down
       end
     end
+
     resources :documents do
       member do
         get :up
